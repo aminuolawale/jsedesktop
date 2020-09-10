47a45a3a469958593816efe94dfc0fbcb6b732bd
Mohammed Aminu <aminuolawale@bitbucket.org>
2020-09-10 02:08:39 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const {is} = require("electron-util");
+
+let window;
+function createWindow() {
+  console.log('create window was called/');
+  window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  window.loadFile('./index.html');
+  if (is.development){
+    window.webContents.openDevTools();
+  }
+  window.on('closed', () => {
+    window = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+// quit when all windows are closed
+app.on('window-all-closed', () => {
+  // on macos only quit when a user explicitly quits the application
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (window == null) {
+    createWindow();
+  }
+});
